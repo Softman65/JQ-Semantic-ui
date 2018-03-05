50b8fb87faf5ced58cc82d1aaed1aa83e2d0afab
Softman65 <rbenitez2012@gmail.com>
2018-03-05 16:11:06 +0100

    pull

--- www_client/public/js/home.js
+++ www_client/public/js/home.js
@@ -8,74 +8,47 @@ $( document ).ready(function() {
 
             const UIMap ={
                 body :[ 
-                    { 
-                        obj: app.semantic.constructor.segment ,
-                        extracss: 'raised',
-                        content:[ 
-                            {   obj: app.semantic.constructor.forms.constructor,
-                                content:[
-                                    { obj:app.semantic.constructor.h4 , css:'ui dividing header', html:'Datos Personales'},
-                                    { obj: app.semantic.constructor.forms.field , 
-                                        content:[
-                                            { obj: app.semantic.constructor.label , html: 'nombre' }, 
-                                            { obj: app.semantic.constructor.forms.fields,
-                                                content:[
-                                                    { obj: app.semantic.constructor.forms.field , 
-                                                        content:[
-                                                            { obj: app.semantic.constructor.forms.input.text ,name:'uno' , placeholder: 'nombre' }
-                                                        ]
-                                                    },
-                                                    { obj: app.semantic.constructor.forms.field , 
-                                                        content:[
-                                                            { obj: app.semantic.constructor.forms.input.text ,name:'uno' , placeholder: 'nombre' }
-                                                        ]
-                                                    }
-                                                ]
-                                            }
-                                        ]
-                                    },
-                                    { obj: app.semantic.constructor.forms.field , 
-                                        content:[ 
-                                            { obj: app.semantic.constructor.forms.fields,
-                                                content:[
-                                                    { obj: app.semantic.constructor.label , html: 'apellidos' },
-                                                    { obj: app.semantic.constructor.forms.input.text ,name:'dos' , placeholder: 'apellidos' }
-                                                ]
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-              //          { 
-              //                  obj: app.semantic.constructor.button.standard ,
-              //                  text:'hi 1',
-              //                  events:{
-              //                      click:function(){
-              //                          alert('hi 1')
-              //                      }
-              //                  },
-              //                  css:'red big' 
-                            
-              //          },
-              //          {append: { 
-              //                  obj: app.semantic.constructor.button.standard ,
-              //                  text:'hi 2',
-              //                  events:{
-              //                      click:function(){
-              //                          alert('hi 2')
-              //                      }
-              //                  } 
-              //              }  
-              //          },
-               //         {append: { 
-               //             obj: app.semantic.constructor.form ,
-               //             fields:[ 
-                //                { label: 'test' , obj: $('<input type="text">') }
-                 //           ] 
-                 //       }  
-                 //   }
+                        { 
+                            obj: app.semantic.constructor.segment ,
+                            extracss: 'raised',
+                            content:[ 
+                                {   obj: app.semantic.constructor.forms.constructor,
+                                    content:[
+                                        { obj:app.semantic.constructor.h4 , css:'ui dividing header', html:'Datos Personales'},
+                                        { obj: app.semantic.constructor.forms.field , 
+                                            content:[
+                                                { obj: app.semantic.constructor.label , html: 'nombre' }, 
+                                                { obj: app.semantic.constructor.forms.fields,
+                                                    content:[
+                                                        { obj: app.semantic.constructor.forms.field , 
+                                                            content:[
+                                                                { obj: app.semantic.constructor.forms.input.text ,name:'uno' , placeholder: 'nombre' }
+                                                            ]
+                                                        },
+                                                        { obj: app.semantic.constructor.forms.field , 
+                                                            content:[
+                                                                { obj: app.semantic.constructor.forms.input.text ,name:'uno' , placeholder: 'nombre' }
+                                                            ]
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        },
+                                        { obj: app.semantic.constructor.forms.field , 
+                                            content:[ 
+                                                { obj: app.semantic.constructor.forms.fields,
+                                                    content:[
+                                                        { obj: app.semantic.constructor.label , html: 'apellidos' },
+                                                        { obj: app.semantic.constructor.forms.input.text ,name:'dos' , placeholder: 'apellidos' }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+
                     ]
                 }
             
